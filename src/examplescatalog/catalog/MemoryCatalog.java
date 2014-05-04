c166660a5394d790cd4e51b316426116282eb20f
Aleks Yablokov <ya_al@bk.ru>
2014-05-04 15:58:18 +0400

    Написал реализацию для MemoryCatalog

--- src/examplescatalog/catalog/MemoryCatalog.java
+++ src/examplescatalog/catalog/MemoryCatalog.java
@@ -1,18 +1,29 @@
 package examplescatalog.catalog;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Каталог примеров, хранимый в памяти.
  */
 public class MemoryCatalog implements ICatalog {
+    private Map<String, Project> projects = new HashMap<>();
+
+    public MemoryCatalog(ICatalog baseCatalog) {
+        for (Project project : baseCatalog.getAllProjects()) {
+            projects.put(project.getId(), project);
+        }
+    }
+
     @Override
     public Project getProjectById(String projectId) {
-        return null;
+        return projects.get(projectId);
     }
 
     @Override
     public List<Project> getAllProjects() {
-        return null;
+        return new ArrayList<>(projects.values());
     }
 }
--- src/examplescatalog/catalog/Project.java
+++ src/examplescatalog/catalog/Project.java
@@ -1,7 +1,21 @@
 package examplescatalog.catalog;
 
+import java.io.File;
+
 /**
  * Пример из каталога примеров.
  */
 public class Project {
+    public String getId() {
+        return null;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public File getFolder() {
+        return null;
+    }
+
 }
