dbd1677ad7cc3f508b5c4f13eb582ade1d4f8a20
Aleks Yablokov <ya_al@bk.ru>
2014-05-04 17:50:41 +0400

    Создал бины examplesRoot и projectFileMasks.

--- src/examplescatalog/application/ExamplesCatalog.java
+++ src/examplescatalog/application/ExamplesCatalog.java
@@ -1,5 +1,6 @@
 package examplescatalog.application;
 
+import examplescatalog.catalog.ICatalog;
 import examplescatalog.settings.ISettings;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -11,5 +12,6 @@ public class ExamplesCatalog {
     public static void main(String[] args) {
         ApplicationContext context = new GenericXmlApplicationContext(ExamplesCatalog.class, "spring.xml");
         ISettings settings = (ISettings) context.getBean("settings");
+        ICatalog catalog = (ICatalog) context.getBean("catalog");
     }
 }
