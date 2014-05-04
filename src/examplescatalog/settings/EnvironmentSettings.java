d750f4a8cbc0a5ddf822b87182778233406b9130
Aleks Yablokov <ya_al@bk.ru>
2014-05-04 15:24:56 +0400

    Заполнение полей в EnvironmentSettings

--- src/examplescatalog/settings/EnvironmentSettings.java
+++ src/examplescatalog/settings/EnvironmentSettings.java
@@ -6,26 +6,33 @@ import java.util.List;
  * Читает настройки из переменных окружения.
  */
 public class EnvironmentSettings implements ISettings {
+    private int port;
+    private String projectIdFilename;
+    private List<String> masks;
+
     /**
      * @param xmlSettings                 Настройки, загруженные из xml (будем их переопределять).
      * @param examplesRootEnvironment     Имя переменной окружения с путем к папке каталога примеров.
      * @param intellijIdeaPathEnvironment Имя переменной окружения с путем к исполняемому файлу IntellijIdea.
      */
     public EnvironmentSettings(ISettings xmlSettings, String examplesRootEnvironment, String intellijIdeaPathEnvironment) {
+        port = xmlSettings.getPort();
+        projectIdFilename = xmlSettings.getProjectIdFilename();
+        masks = xmlSettings.getProjectFileMasks();
     }
 
     @Override
     public int getPort() {
-        return 0;
+        return port;
     }
 
     @Override
     public List<String> getProjectFileMasks() {
-        return null;
+        return masks;
     }
 
     @Override
     public String getProjectIdFilename() {
-        return null;
+        return projectIdFilename;
     }
 }
