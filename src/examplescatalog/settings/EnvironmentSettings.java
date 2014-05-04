678c78ae6093f470a320204773e98ccbebc9e62b
Aleks Yablokov <ya_al@bk.ru>
2014-05-04 15:19:44 +0400

    Имена переменных окружения задаются в конфиге Spring.

--- src/examplescatalog/settings/EnvironmentSettings.java
+++ src/examplescatalog/settings/EnvironmentSettings.java
@@ -6,7 +6,12 @@ import java.util.List;
  * Читает настройки из переменных окружения.
  */
 public class EnvironmentSettings implements ISettings {
-    public EnvironmentSettings(ISettings xmlSettings) {
+    /**
+     * @param xmlSettings                 Настройки, загруженные из xml (будем их переопределять).
+     * @param examplesRootEnvironment     Имя переменной окружения с путем к папке каталога примеров.
+     * @param intellijIdeaPathEnvironment Имя переменной окружения с путем к исполняемому файлу IntellijIdea.
+     */
+    public EnvironmentSettings(ISettings xmlSettings, String examplesRootEnvironment, String intellijIdeaPathEnvironment) {
     }
 
     @Override
