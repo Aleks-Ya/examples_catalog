d3341affca9a141ea1fe3abc4576e84df0324386
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-27 19:11:47 +0400

    Удаление временной папки в тесте

--- test/examplescatalog/catalog/CatalogTest.java
+++ test/examplescatalog/catalog/CatalogTest.java
@@ -1,24 +1,48 @@
 package examplescatalog.catalog;
 
-import examplescatalog.catalog.Catalog;
+import examplescatalog.settings.ISettings;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 import static org.testng.Assert.assertEquals;
 
 public class CatalogTest {
     public static final String TEST_PROFILE = "test";
+    private static AnnotationConfigApplicationContext context;
 
-    //todo удалить временную папку после теста
-    @Test
-    public void testGetProjectFolders() throws Exception {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    @BeforeClass
+    public void beforeClass() {
+        context = new AnnotationConfigApplicationContext();
         context.getEnvironment().addActiveProfile(TEST_PROFILE);
         context.scan("examplescatalog.settings");
         context.scan("examplescatalog.catalog");
         context.refresh();
+    }
+
+    @Test
+    public void testGetProjectFolders() throws Exception {
 
         Catalog catalog = context.getBean(Catalog.class);
         assertEquals(catalog.getAllProjects().size(), 3);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        ISettings settings = context.getBean("settings", ISettings.class);
+        File rootDir = new File(settings.getExamplesRoot());
+        deleteTree(rootDir);
+    }
+
+    private void deleteTree(File... files) {
+        for (File file : files) {
+            if (file.isDirectory()) {
+                deleteTree(file.listFiles());
+            }
+            file.delete();
+        }
     }
 }
