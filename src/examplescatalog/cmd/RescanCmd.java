1c7b0a2d37cd8f5ad8d47cc708362146b37fff42
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-28 16:02:47 +0400

    Добавил логирование в RescanCmd

--- src/examplescatalog/cmd/RescanCmd.java
+++ src/examplescatalog/cmd/RescanCmd.java
@@ -2,24 +2,25 @@ package examplescatalog.cmd;
 
 import examplescatalog.catalog.Pr;
 import examplescatalog.catalog.filesystem.PrFolderScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Команда "Пересканировать каталог проектов".
+ * todo fix Количество проектов плюсуется при повторном сканировании
  */
 @Component("rescan")
 class RescanCmd implements ICmd {
-    private static final List<String> NAMES = Arrays.asList("/scan");
+    private static final Logger LOG = LoggerFactory.getLogger(RescanCmd.class);
 
     @Autowired
     private PrFolderScanner scanner;
 
     @Override
     public void execute(Pr pr) throws CmdException {
+        LOG.info("Rescan catalog folder");
         scanner.scan();
     }
 }
