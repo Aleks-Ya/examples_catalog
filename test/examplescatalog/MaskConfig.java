36d4a02c54a6385a31645285f6c2c6c95e024c36
Aleks Yablokov <ya_al@bk.ru>
2014-06-04 08:03:03 +0400

    Исправил тесты

--- test/examplescatalog/MaskConfig.java
+++ test/examplescatalog/MaskConfig.java
@@ -37,12 +37,17 @@ public class MaskConfig {
     private PrFileMask makeMask(String maskStr, int priority) throws NoSuchFieldException, IllegalAccessException {
         Field maskField = PrFileMask.class.getDeclaredField("mask");
         maskField.setAccessible(true);
+
         Field priorityField = PrFileMask.class.getDeclaredField("priority");
         priorityField.setAccessible(true);
 
-        PrFileMask imlMask = new PrFileMask();
-        priorityField.set(imlMask, priority);
-        maskField.set(imlMask, maskStr);
-        return imlMask;
+        Field prDirTypeField = PrFileMask.class.getDeclaredField("prDirType");
+        prDirTypeField.setAccessible(true);
+
+        PrFileMask mask = new PrFileMask();
+        priorityField.set(mask, priority);
+        maskField.set(mask, maskStr);
+        prDirTypeField.set(mask, PrFileMask.PrDirType.FILE);
+        return mask;
     }
 }
