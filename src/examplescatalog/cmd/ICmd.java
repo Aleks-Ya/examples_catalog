a09f603db0ea1760bc59b22a7afad5d85429fe22
Aleksey Yablokov <ya_al@bk.ru>
2014-06-05 23:20:32 +0400

    todo

--- src/examplescatalog/cmd/ICmd.java
+++ src/examplescatalog/cmd/ICmd.java
@@ -5,6 +5,7 @@ import examplescatalog.catalog.Pr;
 /**
  * Команда, выполняемая в ответ на запрос серверу.
  * todo Добавить командам алиасы
+ * todo Реализовать команду запуска NetBeans
  */
 public interface ICmd {
 
